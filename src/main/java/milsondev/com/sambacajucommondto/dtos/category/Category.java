39ceffb21a31c6b7d6e9e23452116f163e3a2a38
Milson <milsinho.milsinho@gmail.com>
2024-12-19 00:09:49 +0100

    updated pom

--- src/main/java/milsondev/com/sambacajucommondto/dtos/category/Category.java
+++ src/main/java/milsondev/com/sambacajucommondto/dtos/category/Category.java
@@ -1,15 +1,11 @@
-package com.milsondev.stockservice.dtos;
+package milsondev.com.sambacajucommondto.dtos.category;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
 @Data
-@Document(collection = "categories")
 public class Category {
-        @Id
         private UUID id;
         private String categoryName;
         private int qtd;
--- src/main/java/milsondev/com/sambacajucommondto/dtos/product/Product.java
+++ src/main/java/milsondev/com/sambacajucommondto/dtos/product/Product.java
@@ -1,27 +1,15 @@
-package com.milsondev.stockservice.dtos;
+package milsondev.com.sambacajucommondto.dtos.product;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
 @Data
-@Document(collection = "products")
 public class Product {
-    @Id
     private UUID id;
     private String productName;
     private Double price;
     private Double oldPrice;
     private int star;
     private String imageUrl;
-
-    public String getProductName() {
-        final int MAX_LENGTH = 84;
-        if (productName != null && productName.length() > MAX_LENGTH) {
-            return productName.substring(0, MAX_LENGTH) + "...";
-        }
-        return productName;
-    }
 }
